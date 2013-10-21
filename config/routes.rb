22804929ff3b055243a8788959d9de177cba730c
Derik Strattan <derik.strattan@gmail.com>
2013-10-21 15:34:48 -0700

    Using heirships model to define index view and creation method

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,7 @@ Heir::Application.routes.draw do
   resources :people
 
   resources :heirships
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  root 'liabilities#index'
+  root 'heirships#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
